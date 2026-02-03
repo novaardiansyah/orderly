6fe83f3a222707fe8904a53a2cd140b98033c001
novaardiansyah <novaardiansyah78@gmail.com>
2026-02-03 16:59:40 +0700

    style(admin): configure app panel appearance and options
    - Set favicon for app panel
    - Configure sidebar and topbar
    - Adjust additional app panel options

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -10,50 +10,67 @@ use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Widgets\AccountWidget;
-use Filament\Widgets\FilamentInfoWidget;
+use Filament\Support\Enums\Width;
+use Filament\View\PanelsRenderHook;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Support\HtmlString;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 class AppPanelProvider extends PanelProvider
 {
-    public function panel(Panel $panel): Panel
-    {
-        return $panel
-            ->default()
-            ->id('app')
-            ->path('app')
-            ->login()
-            ->colors([
-                'primary' => Color::Amber,
-            ])
-            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
-            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
-            ->pages([
-                Dashboard::class,
-            ])
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
-            ->widgets([
-                AccountWidget::class,
-                FilamentInfoWidget::class,
-            ])
-            ->middleware([
-                EncryptCookies::class,
-                AddQueuedCookiesToResponse::class,
-                StartSession::class,
-                AuthenticateSession::class,
-                ShareErrorsFromSession::class,
-                VerifyCsrfToken::class,
-                SubstituteBindings::class,
-                DisableBladeIconComponents::class,
-                DispatchServingFilamentEvent::class,
-            ])
-            ->authMiddleware([
-                Authenticate::class,
-            ]);
-    }
+  public function panel(Panel $panel): Panel
+  {
+    return $panel
+      ->default()
+      ->brandName(config('app.name'))
+      ->spa()
+      ->id('app')
+      ->path('app')
+      ->login()
+      ->colors([
+        'primary' => Color::Blue,
+      ])
+      ->passwordReset()
+      ->sidebarCollapsibleOnDesktop()
+      ->maxContentWidth(Width::Full)
+      ->topbar(false)
+      ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
+      ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
+      ->pages([
+        Dashboard::class,
+      ])
+      ->favicon(asset('favicon.png'))
+      ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
+      ->widgets([
+        // AccountWidget::class,
+        // FilamentInfoWidget::class,
+      ])
+      ->middleware([
+        EncryptCookies::class,
+        AddQueuedCookiesToResponse::class,
+        StartSession::class,
+        AuthenticateSession::class,
+        ShareErrorsFromSession::class,
+        VerifyCsrfToken::class,
+        SubstituteBindings::class,
+        DisableBladeIconComponents::class,
+        DispatchServingFilamentEvent::class,
+      ])
+      ->authMiddleware([
+        Authenticate::class,
+      ])
+      ->renderHook(
+        PanelsRenderHook::HEAD_END,
+        fn(): HtmlString => new HtmlString('
+          <style>
+            *::-webkit-scrollbar { display: none; }
+            * { -ms-overflow-style: none; scrollbar-width: none; }
+          </style>
+        ')
+      );
+  }
 }
