e58e326832ab9cc4bc079579c2adc41956cc4e81
novaardiansyah <novaardiansyah78@gmail.com>
2026-02-05 13:16:53 +0700

    style(product): refine add and edit ProductCategory forms
    - Adjust add ProductCategory form layout
    - Adjust edit ProductCategory form layout

--- app/Filament/Resources/ProductCategories/Schemas/ProductCategoryForm.php
+++ app/Filament/Resources/ProductCategories/Schemas/ProductCategoryForm.php
@@ -2,15 +2,30 @@
 
 namespace App\Filament\Resources\ProductCategories\Schemas;
 
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class ProductCategoryForm
 {
-    public static function configure(Schema $schema): Schema
-    {
-        return $schema
-            ->components([
-                //
-            ]);
-    }
+  public static function configure(Schema $schema): Schema
+  {
+    return $schema
+      ->components([
+        Section::make()
+          ->description(__('resources/product_categories.sections.general_description'))
+          ->schema([
+            TextInput::make('name')
+              ->label(__('resources/product_categories.columns.name'))
+              ->required()
+              ->maxLength(255),
+            Textarea::make('description')
+              ->label(__('resources/product_categories.columns.description'))
+              ->rows(3)
+              ->required()
+              ->maxLength(1000),
+          ]),
+      ]);
+  }
 }
